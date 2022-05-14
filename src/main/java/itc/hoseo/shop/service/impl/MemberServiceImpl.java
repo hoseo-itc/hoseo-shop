6b0c4a39d65fffc6a5eedb175b643b76f06eba08
Jin Hyun Park <pjh0410@gmail.com>
2022-05-14 11:42:01 +0900

    10주차 실습1 예제 완료 커밋

--- src/main/java/itc/hoseo/shop/service/impl/MemberServiceImpl.java
+++ src/main/java/itc/hoseo/shop/service/impl/MemberServiceImpl.java
@@ -2,21 +2,34 @@ package itc.hoseo.shop.service.impl;
 
 import itc.hoseo.shop.domain.Favorite;
 import itc.hoseo.shop.domain.Member;
+import itc.hoseo.shop.repo.FavoriteRepository;
+import itc.hoseo.shop.repo.MemberRepository;
 import itc.hoseo.shop.service.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @Service
+@Slf4j
+@ControllerAdvice
 public class MemberServiceImpl implements MemberService {
-
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private FavoriteRepository favoriteRepository;
 
     @Override
-    public boolean join(Member m, Favorite... Favorites) {
-        /**
-         * TODO :: 회원가입과 동시에 관심사가 추가되도록 해야함.
-         * 단 관심사는 동일한분야가 두개 가질수 없음
-         * 만일 동일한 관심사가 두개 들어온 경우 예외가 발생할것이며 기존에 가입되었된 회원 가입 자체도 취소되어야함
-         * (취소란 데이터베이스도 롤백 되어야함)
-         */
-        return false;
+    @Transactional
+    public boolean join(Member m, Favorite... favorites) {
+        memberRepository.insert(m);
+        for(Favorite f : favorites){
+            favoriteRepository.insert(f);
+        }
+        return true;
     }
 }
--- src/test/java/itc/hoseo/shop/MemberServiceTests.java
+++ src/test/java/itc/hoseo/shop/MemberServiceTests.java
@@ -1,11 +1,15 @@
 package itc.hoseo.shop;
 
 import itc.hoseo.shop.domain.Favorite;
+import itc.hoseo.shop.domain.Member;
 import itc.hoseo.shop.repo.FavoriteRepository;
 import itc.hoseo.shop.repo.LoginLogRepository;
 import itc.hoseo.shop.repo.MemberRepository;
+import itc.hoseo.shop.service.MemberService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -13,20 +17,43 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Slf4j
 class MemberServiceTests {
 
     @Autowired
-    private FavoriteRepository favoriteRepository;
+    @Qualifier("memberServiceTxImpl")
+    private MemberService memberService;
+
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Test
     void contextLoads() {
-        Favorite f1 = Favorite.builder()
-                .memberSeq(1)
-                .name("코인")
+        Member m1 = Member.builder()
+                .id("luna")
+                .name("shock")
+                .password("나만아니면되")
+                .role("ROLE_USER")
+                .useYn("Y")
                 .build();
-        favoriteRepository.insert(f1);
 
-        assertEquals(1, favoriteRepository.findByMemberSeq(1).size());
+        Favorite f1 = Favorite.builder()
+                .memberSeq(5)
+                .name("주식")
+                .build();
+
+        Favorite f2 = Favorite.builder()
+                .memberSeq(5)
+                .name("부동산")
+                .build();
+
+
+
+        assertEquals(false, memberService.join(m1,f1,f1));
+
+
+        assertEquals(4,memberRepository.findAll().size());
+
 
 
     }
