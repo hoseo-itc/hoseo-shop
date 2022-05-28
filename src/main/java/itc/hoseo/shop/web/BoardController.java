ebb5260624357efb91350fdfecf75d549bfc072c
Jin Hyun Park <pjh0410@gmail.com>
2022-05-28 11:50:42 +0900

    10주차 실습1 예제 완료

--- src/main/java/itc/hoseo/shop/web/BoardController.java
+++ src/main/java/itc/hoseo/shop/web/BoardController.java
@@ -1,26 +1,40 @@
 package itc.hoseo.shop.web;
 
+import itc.hoseo.shop.domain.Board;
+import itc.hoseo.shop.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class BoardController {
+    @Autowired
+    private BoardService boardService;
 
-    @RequestMapping("board/form")
-    public String form(){
+    @RequestMapping(value = "board/form", method = RequestMethod.GET)
+    public String form(@ModelAttribute Board board){
         return "board/form";
     }
 
+    @RequestMapping(value = "board/submit", method = RequestMethod.POST)
+    public String save(Board board, BindingResult bindingResult){
+        boardService.save(board);
+        return "redirect:/board/list?id=" + board.getId();
+    }
+
     @RequestMapping("board/list")
-    public String list(){
+    public String list(@RequestParam String id, ModelMap mm) {
+        mm.put("list", boardService.findById(id));
         return "board/list";
     }
 
-    @RequestMapping("board/detail")
-    public String detail(@RequestParam int seq){
-        return "board/detail";
-    }
+    //@RequestMapping("board/detail")
+    //public String detail(@RequestParam int seq){
+    //    return "board/detail";
+    //}
 
 
 
